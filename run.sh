a9790334c281a83d28365304fa3580fd3b98abf9
Martin Lacroix <lacroix.martin@tutanota.com>
2022-08-17 18:06:30 +0200

    Update

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 # Environment variables
 
 export CPU_PER_PROC=4
-export INPUT=examples/staticNcomp_3D_RBF/input.py
+# export INPUT=examples/damNcomp_2D/input.py
+export INPUT=examples/streamNcomp_2D/input.py
+# export INPUT=examples/staticNcomp_2D/input.py
+# export INPUT=examples/ZiFeiMeng_3D_RBF/input.py
+# export INPUT=examples/MarcoLucio_2D/input.py
 
 # Clean output folder
 
@@ -14,3 +18,4 @@ export MKL_NUM_THREADS=${CPU_PER_PROC}
 export OMP_NUM_THREADS=${CPU_PER_PROC}
 export OPTION="-map-by node:PE=${CPU_PER_PROC}"
 mpiexec ${OPTION} -n 2 python main.py -k ${CPU_PER_PROC} ${INPUT}
+# mpiexec ${OPTION} -n 2 python main.py -k 1 ${INPUT}
