93dbc76eaf7fb844bc74a2d9259ff56ecaf53fb6
Martin Lacroix <lacroix.martin@tutanota.com>
2022-09-23 18:23:28 +0200

    Update

--- examples/MarcoLucio_2D/input_pfem.lua
+++ examples/MarcoLucio_2D/input_pfem.lua
@@ -15,7 +15,7 @@ Problem.maxFactor = 10
 
 Problem.Mesh = {}
 Problem.Mesh.alpha = 1.2
-Problem.Mesh.omega = 0.8
+Problem.Mesh.omega = 0.7
 Problem.Mesh.gamma = 0.6
 Problem.Mesh.hchar = 0.005
 Problem.Mesh.gammaFS = 0.6
