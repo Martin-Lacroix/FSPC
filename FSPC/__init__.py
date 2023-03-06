8c567c4b26f6d28129587f8de91839296ff10699
Martin Lacroix <lacroix.martin@tutanota.com>
2023-03-06 13:55:23 +0100

    Rename

--- FSPC/__init__.py
+++ FSPC/__init__.py
@@ -1,8 +1,6 @@
 from .toolbox import *
-
-from .interpolator.nearestNeigh import KNN
-from .interpolator.basisFunction import RBF
-
-from .algorithm.blockGauss import BGS_ADR
-from .algorithm.multiVector import IQN_MVJ
-from .algorithm.leastSquare import IQN_ILS
+from .interpolator.NearestNeigh import *
+from .interpolator.BasisFunction import *
+from .algorithm.BlockGauss import *
+from .algorithm.MultiVector import *
+from .algorithm.LeastSquare import *
--- run.sh
+++ run.sh
@@ -14,18 +14,18 @@ export PYTHONPATH=${PWD}/../PFEM3D/build/bin:${PYTHONPATH}
 
 export SCRIPT=${PWD}/examples/2D/carsherWall/main.py
 export SCRIPT=${PWD}/examples/2D/coolingDisk/main.py
-export SCRIPT=${PWD}/examples/2D/damBreak/main.py
-export SCRIPT=${PWD}/examples/2D/elasticFunnel/main.py
-export SCRIPT=${PWD}/examples/2D/flowContact/main.py
-export SCRIPT=${PWD}/examples/2D/hydroStatic/main.py
-export SCRIPT=${PWD}/examples/2D/pureConduction/main.py
-export SCRIPT=${PWD}/examples/2D/rubberGate/main.py
-export SCRIPT=${PWD}/examples/2D/thermoSquare/main.py
+# export SCRIPT=${PWD}/examples/2D/damBreak/main.py
+# export SCRIPT=${PWD}/examples/2D/elasticFunnel/main.py
+# export SCRIPT=${PWD}/examples/2D/flowContact/main.py
+# export SCRIPT=${PWD}/examples/2D/hydroStatic/main.py
+# export SCRIPT=${PWD}/examples/2D/pureConduction/main.py
+# export SCRIPT=${PWD}/examples/2D/rubberGate/main.py
+# export SCRIPT=${PWD}/examples/2D/thermoSquare/main.py
 
-export SCRIPT=${PWD}/examples/3D/coolingDisk/main.py
-export SCRIPT=${PWD}/examples/3D/crossFlow/main.py
-export SCRIPT=${PWD}/examples/3D/damBreak/main.py
-export SCRIPT=${PWD}/examples/3D/hydroStatic/main.py
+# export SCRIPT=${PWD}/examples/3D/coolingDisk/main.py
+# export SCRIPT=${PWD}/examples/3D/crossFlow/main.py
+# export SCRIPT=${PWD}/examples/3D/damBreak/main.py
+# export SCRIPT=${PWD}/examples/3D/hydroStatic/main.py
 
 # Clean output folder
 
