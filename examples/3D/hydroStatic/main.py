d1964c461a2acde28a723275aa1524b53496f137
Martin Lacroix <lacroix.martin@tutanota.com>
2023-06-27 21:14:59 +0200

    Update main.py

--- examples/3D/hydroStatic/main.py
+++ examples/3D/hydroStatic/main.py
@@ -1,20 +1,16 @@
 import os.path as path
-import numpy as np
 import FSPC
 
 # %% Input Parameters for FSPC
 
 pathF = path.dirname(__file__)+'/inputF.lua'
 pathS = path.dirname(__file__)+'/inputS.py'
-RBF = lambda r: np.square(r)*np.ma.log(r)
 
 # %% Initialize the Simulation
 
 FSPC.setConvMech(1e-8)
 FSPC.setStep(1e-2,0.05)
 FSPC.setSolver(pathF,pathS)
-#FSPC.setInterp(FSPC.interpolator.RBF,RBF)
-
 FSPC.setInterp(FSPC.interpolator.ETM,9)
 
 # Configure the algorithm
